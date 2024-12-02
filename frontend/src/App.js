ead1eaa686afec370f61cbf38de629ffbbb5029e
cameronjbailey <cameronbailey@gmail.com>
2024-12-02 15:47:36 -0500

    Initial commit for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>StageCraft Testing</h1>
+      <p>Humble beginnings for testing practice.</p>
+    </div>
+  );
+}
+
+export default App;
