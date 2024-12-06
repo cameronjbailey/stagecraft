fef7f97cb6a3787cdbec8060a3848a8a52cec90d
cameronjbailey <cameronbailey@gmail.com>
2024-12-05 21:43:43 -0500

    Connect to api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState('Loading...');
+
+  useEffect(() => {
+    fetch('https://stagecraft-330z.onrender.com/api/welcome')
+      .then(response => response.text())
+      .then(data => setMessage(data));
+  }, []);
+  
   return (
     <div className="App">
       <h1>StageCraft Preview</h1>
       <p>Humble beginnings for testing practice.</p>
       <p>New content and features are coming soon.</p>
+      <p>{message}</p>
     </div>
   );
 }
